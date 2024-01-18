7fbeeb4478e2dbd0bfb4e7b1f5a3b081b6d4af76
Alexandr Abramov <alexandr.abramovvv@yandex.ru>
2024-01-18 16:10:40 +0300

    Add drf-spectacular to document the project

--- config/settings.py
+++ config/settings.py
@@ -42,8 +42,27 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework",
     "django_extensions",
+    "drf_spectacular",
+    "drf_spectacular_sidecar",
     "user_selection.apps.UserSelectionConfig",
 ]
+
+# Register spectacular AutoSchema with DRF.
+# https://drf-spectacular.readthedocs.io/en/latest/readme.html#installation
+REST_FRAMEWORK = {
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+}
+
+
+SPECTACULAR_SETTINGS = {
+    "TITLE": '"Compas PRO" user selection project',
+    "DESCRIPTION": "Test task for Compas PRO",
+    "VERSION": "1.0.0",
+    "SERVE_INCLUDE_SCHEMA": False,
+    "SWAGGER_UI_DIST": "SIDECAR",  # shorthand to use the sidecar instead
+    "SWAGGER_UI_FAVICON_HREF": "SIDECAR",
+    "REDOC_DIST": "SIDECAR",
+}
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
