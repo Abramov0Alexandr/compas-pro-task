718910f26148dae4f3382f02e77b512a780174f3
Alexandr Abramov <alexandr.abramovvv@yandex.ru>
2024-01-18 09:55:17 +0300

    Refactoring after linter checkout

--- user_selection/apps.py
+++ user_selection/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class UserSelectionConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'user_selection'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "user_selection"
